ba196af2807821fa7cbd8e1bcb2e10a3b7ba016d
Joao Paulo Sanchez <joaopaulosanchez@192.168.1.18>
2025-01-14 16:53:12 -0300

    feat: add readme.md

--- src/test/java/com/puppies/TestcontainersConfiguration.java
+++ src/test/java/com/puppies/TestcontainersConfiguration.java
@@ -4,7 +4,10 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
@@ -12,7 +15,10 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest")).waitingFor(
+                Wait.forLogMessage(".*database system is ready to accept connections.*\\s", 2)
+                        .withStartupTimeout(Duration.ofSeconds(30))
+        );
     }
 
 }
